987636637b612800a869970546d3212ecee4070d
Aaron Ballard <mraballard@gmail.com>
2016-08-18 13:26:50 -0300

    Deleted duplicate files, added javascript to add divs for gameboard and cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,11 +5,34 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+//Add card divs to gameboard
+var gameBoard = document.getElementById('game-board');
+  for (var i = 0; i < 4; i++) {
+    var newCard = document.createElement('div');
+//    console.log(newCard);
+    newCard.className = 'card';
+    gameBoard.appendChild(newCard);
+  }
+
+//Function to create HTML for cards. Then, append to div with class board.
+var createBoard = function()  {
+  for (var i = 0; i < 4; i++) {
+    var cards = document.getElementsByClassName('card');
+    cards[i].innerHTML = '';
+  }
+}
+
+
+
+
+
+
 //User clicks card. selected card
 
-if (cardTwo===cardFour) {
+/*if (cardTwo===cardFour) {
   alert("You found a match!");
 }
   else {
     alert("Sorry, try again.");
 }
+*/
